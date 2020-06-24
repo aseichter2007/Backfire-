992ada9519c8d200cdbb56639ea3fb17844e7d8b
aseichter2007 <aseichter2007@gmail.com>
2020-06-24 13:57:41 -0500

    added backend code to source control.

---
+++ Backend/BackfireBackend/BackfireBackend/Controllers/ValuesController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BackfireBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ValuesController>/routing
+        [HttpPost("{routing}")]
+        public void Post(string routing, [FromBody] FileStream file)
+        {
+            string currentLocation = Directory.GetCurrentDirectory();
+            string fileRoute = routing.Replace('+', '/');
+            int fileNumber = 0;
+            while (System.IO.File.Exists(currentLocation+fileRoute+fileNumber+".wav"))
+            {
+                fileNumber++;
+            }
+
+
+
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
